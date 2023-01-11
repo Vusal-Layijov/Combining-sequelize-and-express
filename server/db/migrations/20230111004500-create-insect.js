8875d120f9cefc2592f128e53aa663c1bbd72b2a
Vusal Layijov <Layijovvusal@gmail.com>
2023-01-10 17:03:42 -0800

    Last

---
+++ server/db/migrations/20230111004500-create-insect.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Insects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      territory: {
+        type: Sequelize.STRING
+      },
+      fact: {
+        type: Sequelize.STRING
+      },
+      millimets: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Insects');
+  }
+};
